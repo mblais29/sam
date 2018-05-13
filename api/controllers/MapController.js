1f064c83d3914b2c70987502ba74bb83718e5d36
mblais29 <mblais29@gmail.com>
2018-05-13 15:27:50 +0100

    removed the ability to draw and save to the static property layer, need to develop a way to save a new record to the specified layer.

--- api/controllers/MapController.js
+++ api/controllers/MapController.js
@@ -19,69 +19,6 @@ module.exports = {
 		}
 	},
 	
-	'saveProperty': function(req,res,next){
-		var pg = require('pg');
-		var pgconnection = new pg.Client({
-			user: sails.config.connections.postgresServer.user,
-		  	host: sails.config.connections.postgresServer.host,
-		  	database: sails.config.connections.postgresServer.database,
-		  	password: sails.config.connections.postgresServer.password,
-		  	port: sails.config.connections.postgresServer.port,
-		});
-
-		var geometry = req.param('geom');
-		var parseJSON = JSON.parse(geometry);
-		var queryString = 'INSERT INTO public.property_objects (property_id, geom) VALUES (' + req.param('id') + ', ST_GeomFromGeoJSON(\'' + parseJSON + '\'))';
-
-		pgconnection.connect(function(err, client, done){
-			client.query(queryString, function(err, result){
-				if(err){
-					AlertService.error(req, JSON.stringify(err));
-					return res.ok(false);
-				};
-				client.end();
-				AlertService.success(req, 'Property created successfully...');
-				return res.ok(true);
-
-			});
-		});
-	},
-	
-	'getPropertyRecords': function(req,res,next){
-		Properties.find().where({id: req.param('property_id')}).populateAll().exec(function foundProperty(err,data){
-			if(err) return next(err);
-			return res.ok(data);
-		});
-	},
-	
-	'getAllPropertyRecords': function(req,res,next){
-		Properties.find().populateAll().exec(function foundProperties(err,data){
-			if(err) return next(err);
-			return res.ok(data);
-		});
-	},
-	
-	'getSelectedProperties': function(req,res,next){
-		var pg = require('pg');
-		var pgconnection = new pg.Client({
-			user: sails.config.connections.postgresServer.user,
-		  	host: sails.config.connections.postgresServer.host,
-		  	database: sails.config.connections.postgresServer.database,
-		  	password: sails.config.connections.postgresServer.password,
-		  	port: sails.config.connections.postgresServer.port,
-		});
-		
-		var queryString = 'SELECT property_id, ST_AsGeoJSON(geom) as geometry FROM public.property_objects WHERE property_id = ' + req.param('property_id');
-		
-		pgconnection.connect(function(err, client, done){
-			client.query(queryString, function(err, records){
-			    if(err) return res.json(err);
-			    client.end();
-			    return res.json(records);
-			});
-		});
-	},
-	
 	'getAllMapLayers': function(req,res,next){
 		Maplayers.find().populateAll().exec(function foundLayers(err,data){
 			if(err) return next(err);
--- assets/js/global/map/map.js
+++ assets/js/global/map/map.js
@@ -4,17 +4,12 @@
 var mapOnLoadCenter = new L.LatLng(49.8880,-119.4960); //new L.LatLng(56.1304, -106.3468);
 var mapOnLoadZoom = 15; //4;
 var navBarMargin = 52;
-var propertyList = [];
 var layerControl = "";
 var currentLayers = [];
 var overlayLayers = {};
 var editedLayerProperties = {};
 
 var map = "";
-var mapProperties = L.featureGroup();
-var propertyMarkers = L.featureGroup();
-var propertyLayer = new L.geoJson();
-var propertyGroup = L.featureGroup();
 
 
 if($('body').is('#mapBody')){
@@ -81,7 +76,8 @@ if($('body').is('#mapBody')){
                     clickable: false
                 }
             },*/
-            marker: true
+            marker: true,
+            circlemarker: false
         },
         edit: {
             featureGroup: editableLayers, //REQUIRED!!
@@ -92,7 +88,7 @@ if($('body').is('#mapBody')){
     var drawControl = new L.Control.Draw(options);
     map.addControl(drawControl);
     
-	var propertyControl = L.Control.extend({
+	var mapToolControls = L.Control.extend({
 	 
 	  options: {
 	    position: 'bottomright' 
@@ -103,10 +99,7 @@ if($('body').is('#mapBody')){
 	    container.style.backgroundColor = 'white';
 	    container.style.padding = '2px';
 	    var insideDiv = "";
-	    insideDiv += '<a href="#" title="Zoom to All Properties" onclick="zoomToAllProperties(); return false" ><i class="fa fa-map-marker fa-2x" aria-hidden="true" style="color:black"></i></a>';
 	    insideDiv += '<a href="#" title="Reset View" onclick="resetMapView(); return false"><i class="fa fa-expand fa-2x" aria-hidden="true" style="color:black; padding:3px"></i></a>';
-	    insideDiv += '<a href="#" id="locateProperties" title="Locate Property" data-toggle="modal" data-target="#locate-property" ><i class="fa fa-arrows fa-2x" aria-hidden="true" style="color:black; padding:3px"></i></a>';
-	    insideDiv += '<a href="#" onclick="removeSearchedLayers(); return false" title="Delete Located Properties" ><i class="fa fa-trash-o fa-2x" aria-hidden="true" style="color:black; padding:3px"></i></a>';
 	    
 	    container.innerHTML = insideDiv;
 
@@ -115,23 +108,12 @@ if($('body').is('#mapBody')){
 	 
 	});
 	
-	map.addControl(new propertyControl());
+	map.addControl(new mapToolControls());
 
     map.on('draw:created', function (e) {
 		var type = e.layerType,
 			layer = e.layer;
 		var geojson = layer.toGeoJSON();
-		propertyGeojsonGeometry = JSON.stringify(geojson.geometry);
-		
-		var content = getPopupContent(layer);
-        if (content !== null) {
-        	console.log(content);
-            //layer.bindPopup(content);
-        }
-		
-		var content = '<div class="form-group"><div class="ui-widget"><input type="text" class="form-control" id="search-properties" placeholder="Enter property name..."></div></div>';
-
-        layer.bindPopup('<div id="propPopup">' + content + '</br><button type="button" class="btn btn-success" onclick="saveProperty()">Save</button></div>');
 
 		editableLayers.addLayer(layer);
 		layerControl.addOverlay(editableLayers, 'Drawn Asset');
@@ -144,18 +126,14 @@ if($('body').is('#mapBody')){
 
      	if(geojson.features[0].geometry.type == "polygon" || geojson.features[0].geometry.type == "Polygon"){
      		geojson.features[0].properties = editedLayerProperties;
+     		editableLayers.clearLayers();
      	}
-
+		
      	saveEditedLayer(geojson, e);
-     	propertyGeojsonGeometry = JSON.stringify(geojson.features[0].geometry);
 	});
 	
 	map.on('draw:deleted', function (e) {
 		layerControl.removeLayer(editableLayers);
-	});
-	
-	map.on('popupopen', function (e) {
-	    initializeAutoComplete();
 	});
 	
 	map.on('moveend', function(){
@@ -180,24 +158,7 @@ if($('body').is('#mapBody')){
             results.addLayer(L.marker(data.results[i].latlng));
         }
     });
-    
-   getProperties();
-   getPropertyLocations();
-   
-   $('body#mapBody').append('<div id="locate-property" class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button><h4 class="modal-title">Locate Property</h4></div><div class="modal-body"><div id="search-canvas"><table id="table-property-records" class="table table-striped" data-sorting="true" data-filtering="true"><thead><tr><!--- Must have data-type="html" on all data-breakpoints="all" or <ahref=""> table data in Footable Plugin ---><th>Client</th><th>Address</th></tr></thead><tbody></tbody></table></div></div><div class="modal-footer"><button type="button" id="propertySearchSubmit" data-dismiss="modal" class="btn btn-default pull-right ">Submit</button><button type="button" id="propertySearchClose" data-dismiss="modal" class="btn btn-default pull-right btn-close">Close</button></div></div></div></div>');
-
-   $('#propertySearchClose').on('click', function(){
-		$('#locate-property').modal('toggle');
-	});
 	
-   $('#propertySearchSubmit').on('click', function(){
-		getPropertyData();
-	});
-
-	populatePropertySearch();
-	
-	
-
 };
 
 /****************************************
@@ -211,22 +172,7 @@ function resize(){
 }
 
 function resetMapView(){
-	/*if(map.hasLayer(mapProperties) || map.hasLayer(propertyMarkers)){
-		layerControl.removeLayer(mapProperties);
-		layerControl.removeLayer(propertyMarkers);
-		map.removeLayer(mapProperties);
-		map.removeLayer(propertyMarkers);
-		mapProperties = L.featureGroup();
-		propertyMarkers = L.featureGroup();
-	}*/
 	map.setView(mapOnLoadCenter, mapOnLoadZoom);
-}
-
-function removePanel(){
-	$('.close-upload-panel').on('click',function(){
-		var effect = $(this).data('effect');
-	    $(this).closest('.panel')[effect]();
-	});
 }
 
 // Truncate value based on number of decimals
@@ -238,202 +184,6 @@ var _round = function(num, len) {
 var strLatLng = function(latlng) {
     return "("+_round(latlng.lat, 6)+", "+_round(latlng.lng, 6)+")";
 };
-
-var getPopupContent = function(layer) {
-    // Marker - add lat/long
-    if (layer instanceof L.Marker || layer instanceof L.CircleMarker) {
-        return strLatLng(layer.getLatLng());
-    // Circle - lat/long, radius
-    } else if (layer instanceof L.Circle) {
-        var center = layer.getLatLng(),
-            radius = layer.getRadius();
-        return "Center: "+strLatLng(center)+"<br />"
-              +"Radius: "+_round(radius, 2)+" m";
-    // Rectangle/Polygon - area
-    } else if (layer instanceof L.Polygon) {
-        var latlngs = layer._defaultShape ? layer._defaultShape() : layer.getLatLngs(),
-            area = L.GeometryUtil.geodesicArea(latlngs);
-        return "Area: "+L.GeometryUtil.readableArea(area, true);
-    // Polyline - distance
-    } else if (layer instanceof L.Polyline) {
-        var latlngs = layer._defaultShape ? layer._defaultShape() : layer.getLatLngs(),
-            distance = 0;
-        if (latlngs.length < 2) {
-            return "Distance: N/A";
-        } else {
-            for (var i = 0; i < latlngs.length-1; i++) {
-                distance += latlngs[i].distanceTo(latlngs[i+1]);
-            }
-            return "Distance: "+_round(distance, 2)+" m";
-        }
-    }
-    return null;
-};
-
-function getProperties(){
-	$.ajax('/properties/getRecords',{
-      success: function(data) {
-      	for(var i = 0; i < data.length; i++){
-      		propertyList.push('[' + data[i].id +'] - ' + data[i].client[0].client);
-      	}
-      },
-      done: function(data){
-      	
-      },
-      error: function(err) {
-         console.log(err);
-      }
-    });
-};
-
-function initializeAutoComplete(){
-	$( "#search-properties" ).autocomplete();
-        
-    $( "#search-properties" ).autocomplete({
-	  source: propertyList
-	});
-};
-
-function saveProperty(){
-	var record = $('#search-properties').val();
-	
-	var regex = /\[(.*?)\]/g;
-	var newStr = record.match(regex);
-	var id = "";
-	if(newStr != null){
-		id = newStr[0].replace(/[\[\]']+/g, '');
-	}
-	//console.log(id);
-	//console.log(propertyGeojsonGeometry);
-	
-	$.ajax('/map/saveProperty?id=' + id + '&geom=' + JSON.stringify(propertyGeojsonGeometry),{
-      success: function(data) {
-      	location.reload();
-      },
-      done: function(data){
-      	
-      },
-      error: function(err) {
-         console.log(err);
-      }
-    });
-}
-
-function getPropertyLocations(){
-	$.ajax('/properties/getPropertyLocations',{
-      success: function(data) {
-      	var properties = data.rows;
-		for(var i = 0; i < properties.length; i++){
-			var propertyGeojson = JSON.parse(properties[i].geojson);
-			var propertyId = properties[i].property_id;
-			
-			var property = L.geoJSON(propertyGeojson, {
-			   	onEachFeature: function (feature, layer) {
-			   		if(feature.type === 'Polygon' || feature.type === 'MultiPolygon'){
-			   			/*var centroid = layer.getBounds().getCenter();
-			   			var marker = L.marker(centroid);
-			   			if(!map.hasLayer(propertyMarkers)){
-			   				propertyMarkers.addLayer(marker);
-			   			}*/
-			   			createPropertyPopup(propertyId, layer);
-			   		}
-			   		if(feature.type === 'Point'){
-			   			createPropertyPopup(propertyId, layer);
-			   		}
-			   	}
-			});
-			if(!map.hasLayer(mapProperties)){
-				mapProperties.addLayer(property);
-			}
-		}
-		createProperty();
-      },
-      done: function(data){
-
-      },
-      error: function(err) {
-         console.log(err);
-      }
-    });
-}
-
-function createProperty(){
-	if(!map.hasLayer(mapProperties) || map.hasLayer(propertyMarkers)){
-		layerControl.addOverlay(mapProperties, 'Properties');
-		/*layerControl.addOverlay(propertyMarkers, 'Property Markers');*/
-	}
-	
-}
-
-function zoomToAllProperties(){
-	map.fitBounds(mapProperties.getBounds());
-	map.addLayer(mapProperties);
-}
-
-function createPropertyPopup(property_id, layer){
-	$.ajax('/map/getPropertyRecords?property_id=' + property_id,{
-      success: function(data) {
-
-      	var popupInfo = "";
-      		popupInfo += '<b>Client: </b>' + data[0].client[0].client + '</br>';
-      		popupInfo += '<b>Address: </b>' + data[0].address + '</br>';
-      		popupInfo += '<b>Description: </b>' + data[0].description + '</br>';
-
-      	layer.bindPopup('<div class="propertyPopupDiv">' + popupInfo + '</div>');
-      },
-      done: function(data){
-
-      },
-      error: function(err) {
-         console.log(err);
-      }
-    });
-}
-
-function populatePropertySearch(){
-	$.ajax('/map/getAllPropertyRecords',{
-      success: function(data) {
-      	if(data.length){
-      		$('#table-property-records tbody').empty();
-      	}
-      	for(var i = 0; i < data.length; i++){
-      		var addRow = create_table_row(data[i]);
-      		$('#table-property-records').append(addRow);
-      	}
-      	$('#table-property-records').footable().trigger('footable_initialize');
-      },
-      done: function(data){
-
-      },
-      error: function(err) {
-         console.log(err);
-      }
-    });
-};
-
-function create_table_row (data) {
-	var row = $('<tr><td><div class="form-check"><label class="form-check-label"><input class="property-check-input" type="checkbox" value="' + data.id + '"></label> ' + data.client[0].client + '</div></td><td>' + data.address + '</td></tr>');
-    return row;
-}
-
-function getPropertyData(){
-	$(".property-check-input").each(function() {
-        var propertyId = $(this).val();
-        if($(this).prop('checked')){
-	        $.ajax('/map/getSelectedProperties?property_id=' + propertyId,{
-		      success: function(data) {
-		      	convertToLayer(data);
-		      },
-		      done: function(data){
-		
-		      },
-		      error: function(err) {
-		         console.log(err);
-		      }
-		    });
-	    }
-    });
-}
 
 function convertToLayer(data){
 	var properties = data.rows;
@@ -469,12 +219,6 @@ function convertToLayer(data){
 	}
 	propertyGroup.addTo(map);
 	map.fitBounds(propertyGroup.getBounds());
-}
-
-function removeSearchedLayers(){
-	layerControl.removeLayer(propertyGroup);
-	map.removeLayer(propertyGroup);
-	propertyGroup = L.featureGroup();
 }
 
 function getAllMapLayers(){
@@ -828,7 +572,6 @@ function renderLayers(activeLayers){
 }
 
 function saveEditedLayer(geojson, e){
-	console.log(geojson);
 	var newMultiPolygon = {};
 	var updatedLayerGeojsonGeometry = {};
 	var updatedLayerGeojsonId = "";
